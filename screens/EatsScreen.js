290e5cda141048a36de0726f45700e67e503da5a
dev0598 <devansh.parikh@ieee.org>
2021-08-03 19:41:04 +0530

    initial commit

---
+++ screens/EatsScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+
+const EatsScreen = () => {
+    return (
+        <View style={tw`items-center content-center`}>
+            <Text style={tw`text-xl text-black font-bold p-20 text-center`}>Work in Progress, please come back soon!</Text>
+        </View>
+    )
+}
+
+export default EatsScreen
+
+const styles = StyleSheet.create({})
